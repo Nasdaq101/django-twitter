eec25bc4011826e66384f234d1fa06037a5cd7a5
Nasdaq101 <yunfeizhou1015@hotmail.com>
2023-11-05 15:38:14 +0800

    initial vagrant

--- twitter/settings.py
+++ twitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's%q7u37k$f%o6%cq3y=1_qxbnj@bagg4ks+lk%e=&l5xkhpfom'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','192.168.33.10','localhost']
 
 
 # Application definition
