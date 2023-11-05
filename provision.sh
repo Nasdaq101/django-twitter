8a63622cf39b439eb07e92b1212e706eb7fecfa0
vagrant <vagrant@vagrant.vm>
2023-11-05 06:04:29 +0000

    initial vagrant

--- provision.sh
+++ provision.sh
@@ -33,7 +33,7 @@ fi
 pip install --upgrade setuptools -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install --ignore-installed wrapt -i https://pypi.tuna.tsinghua.edu.cn/simple
 # 安装pip最新版
-pip install -U pip 
+pip install -U pip -i https://pypi.tuna.tsinghua.edu.cn/simple
 # 根据 requirements.txt 里的记录安装 pip package，确保所有版本之间的兼容性
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
